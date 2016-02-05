427aa706378a57398ca7999295c2d5e34dc5f7a1
Maciej Rosiek <maciej-rosiek@users.noreply.github.com>
2016-02-05 16:19:31 +0100

    Update init-arch.sh

--- init-arch.sh
+++ init-arch.sh
@@ -13,7 +13,7 @@ echo '[infinality-bundle]' | sudo tee --append /etc/pacman.conf
 echo 'Server = http://bohoomil.com/repo/$arch' | sudo tee --append /etc/pacman.conf
 
 sudo pacman -Syu
-sudo pacman -S i3 xorg xorg-xinit vim git rxvt-unicode rxvt-unicode-terminfo chromium gedit lxappearance arandr alsa-firmware alsa-utils alsa-plugins pulseaudio-alsa pulseaudio thunar thunar-volman thunar-archive-plugin numix-frost-themes numix-icon-theme-square zsh gnome-keyring infinality-bundle networkmanager network-manager-applet udiskie acpi
+sudo pacman -S i3 xorg xorg-xinit vim git chromium gedit lxappearance arandr alsa-firmware alsa-utils alsa-plugins pulseaudio-alsa pulseaudio thunar thunar-volman thunar-archive-plugin numix-frost-themes numix-icon-theme-square zsh gnome-keyring infinality-bundle networkmanager network-manager-applet udiskie acpi
 sudo pacman -R vi
 
 echo '# autostart systemd default session on tty1' | sudo tee --append /etc/profile
@@ -23,23 +23,23 @@ echo 'fi' | sudo tee --append /etc/profile
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-cd ~/dev/dotfiles
-cp oh-my-zsh-themes/my.zsh-theme ~/.oh-my-zsh/themes
-cp .screenrc ~/
-cp .yaourtrc ~/
-cp .zshrc ~/
-cp .Xdefaults ~/
-cp .vimrc ~/
-cp .xinitrc ~/
+cp ~/dev/dotfiles/oh-my-zsh-themes/my.zsh-theme ~/.oh-my-zsh/themes
+cp ~/dev/dotfiles/.screenrc ~/
+cp ~/dev/dotfiles/.yaourtrc ~/
+cp ~/dev/dotfiles/.zshrc ~/
+cp ~/dev/dotfiles/.Xdefaults ~/
+cp ~/dev/dotfiles/.vimrc ~/
+cp ~/dev/dotfiles/.xinitrc ~/
 mkdir -p ~/.config/i3
 mkdir ~/.config/i3status
-cp .config/i3/config ~/.config/i3/
-cp .config/i3status/config ~/.config/i3status/
-cp .config/.i3blocks.conf ~/.config
-yaourt vim-jellybeans
-yaourt i3blocks
-yaourt dmenu2
-yaourt adobe-source-code-pro-fonts 
-yaourt adobe-source-sans-pro-fonts 
+cp ~/dev/dotfiles/.config/i3/config ~/.config/i3/
+cp ~/dev/dotfiles/.config/i3status/config ~/.config/i3status/
+cp ~/dev/dotfiles/.config/.i3blocks.conf ~/.config
+yaourt -S vim-jellybeans
+yaourt -S i3blocks
+yaourt -S dmenu2
+yaourt -S ttf-vista-fonts
+yaourt -S rxvt-unicode-fontspacing-noinc-vteclear-secondarywheel
+sudo pacman -S rxvt-unicode-terminfo
 #yaourt intellij-idea-ultimate-edition
 #yaourt unity-editor
