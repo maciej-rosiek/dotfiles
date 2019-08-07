afee0ad1bf5c77b40f24c98a16100c2a542b201f
Maciej Rosiek <maciej.rosiek.external@zalando.de>
2019-08-07 09:24:42 +0200

    Shuffled files a bit

---
+++ arch/README.md
@@ -0,0 +1,7 @@
+common dot files
+
+    0. Install Antergos base (https://antergos.com)
+    1. Login
+    2. mkdir ~/dev
+    3. git clone https://github.com/maciej-rosiek/dotfiles.git ~/dev/dotfiles
+    4. cd ~/dev/dotfiles/arch; init-arch.sh
---
+++ arch/init-arch.sh
@@ -0,0 +1,46 @@
+
+#http://tutos.readthedocs.org/en/latest/source/Arch.html
+
+#yaourt update-pacman-mirrorlist
+#sudo update-pacman-mirrorlist
+
+sudo mkdir /root/.gnupg
+sudo touch dirmngr_ldapservers.conf
+sudo pacman-key --refresh-keys
+sudo pacman-key -r 962DDE58
+sudo pacman-key --lsign-key 962DDE58
+
+sudo pacman -Syu
+sudo pacman -S i3 xorg xorg-xinit vim git chromium gedit lxappearance arandr alsa-firmware alsa-utils alsa-plugins pulseaudio-alsa pulseaudio thunar thunar-volman thunar-archive-plugin numix-frost-themes numix-icon-theme-square zsh gnome-keyring networkmanager network-manager-applet udiskie acpi
+sudo pacman -R vi
+
+echo '# autostart systemd default session on tty1' | sudo tee --append /etc/profile
+echo 'if [[ "$(tty)" == '/dev/tty1' ]]; then' | sudo tee --append /etc/profile
+echo 'exec startx' | sudo tee --append /etc/profile
+echo 'fi' | sudo tee --append /etc/profile
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+cp ~/dev/dotfiles/oh-my-zsh-themes/my.zsh-theme ~/.oh-my-zsh/themes
+cp ~/dev/dotfiles/.screenrc ~/
+cp ~/dev/dotfiles/.yaourtrc ~/
+cp ~/dev/dotfiles/.zshrc ~/
+cp ~/dev/dotfiles/.Xdefaults ~/
+cp ~/dev/dotfiles/.vimrc ~/
+cp ~/dev/dotfiles/.xinitrc ~/
+mkdir -p ~/.config/i3
+mkdir ~/.config/i3status
+cp ~/dev/dotfiles/.config/i3/config ~/.config/i3/
+cp ~/dev/dotfiles/.config/i3status/config ~/.config/i3status/
+cp ~/dev/dotfiles/.config/.i3blocks.conf ~/.config
+yaourt -S vim-jellybeans
+yaourt -S i3blocks
+yaourt -S dmenu2
+yaourt -S ttf-vista-fonts
+yaourt -S rxvt-unicode-fontspacing-noinc-vteclear-secondarywheel
+yaourt -S playerctl
+sudo pacman -S rxvt-unicode-terminfo
+sudo systemctl enable NetworkManager
+
+#yaourt intellij-idea-ultimate-edition
+#yaourt unity-editor
