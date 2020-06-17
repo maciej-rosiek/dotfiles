e96dbba03dcc617055ce0e747f836fe25af6e1ae
Maciej Rosiek <maciej.rosiek@gmail.com>
2020-06-17 21:57:47 +0200

    Dots

---
+++ arch/.screenlayout/home-1080p.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP --mode 1920x1080 --right-of DisplayPort-0 --pos 3440x542 --rotate normal --brightness .5 --output HDMI-A-0 --off --output DisplayPort-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal
---
+++ arch/.screenlayout/home-wide.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP --right-of DisplayPort-0 --mode 1920x1080 --pos 3440x542 --rotate normal --brightness .5 --output HDMI-A-0 --off --output DisplayPort-0 --primary --mode 3440x1440 --pos 0x0 --rotate normal
