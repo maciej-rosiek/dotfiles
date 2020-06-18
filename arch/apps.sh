6a18514831f3bf727e5ab6c22eac2eecdbf0cde7
Maciej Rosiek <maciej.rosiek@gmail.com>
2020-06-18 15:04:36 +0200

    Dots

---
+++ arch/apps.sh
@@ -0,0 +1,54 @@
+yay vim-hybrid
+yay i3blocks
+yay i3blocks
+yay fontawesome
+yay font-awesome
+yay ponymix
+pacman -S xclip
+sudo pacman -S xclip
+sudo pacman -S picom
+yay tpm
+yay gstreamer-vaapi
+yay gstreamer-plugins-bad
+yay mesa-vdpau
+yay libva-mesa-driver
+yay mesa
+yay vulkan-radeon
+yay xf86-video-amdgpu
+yay blueman
+yay pulseaudio-bluetooth
+yay bluez
+yay bluez-utils
+yay pulseaudio-bluetooth
+yay hsetroot
+yay tlp
+yay tlpui-git
+yay docker
+yay postgres
+yay urxvt-clipboard
+yay urxvt-url-select
+yay urxvt
+yay strace
+yay gThumb
+yay firefox
+yay xorg-xinit
+yay maim
+yay numix
+yay thunar
+yay thunar-archive
+yay dunst
+yay intellij
+yay imwheel
+yay zenity
+yay terraform
+yay awscli
+yay pip
+yay python
+yay aws-iam-authenticator
+yay kubectl
+yay kustomize
+yay kubeseal
+yay keepassx
+yay xcwd
+sudo pacman -R eos-log-tool
+sudo pacman -R eos-update-notifier
